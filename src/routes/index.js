812b166b5de1c2ade851336ab0f0e377076855e8
Collins Njau <collinsnjau39@gmail.com>
2018-08-05 12:35:45 +0300

    feat(src): implements the requests page main panel header (UI only) - create component to render the header - create the view contain the header component [Delivers #159393234]

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import { Route, Switch } from "react-router-dom";
 
 import IndexPage from "../views/IndexPage/IndexPage";
 import Login from "../views/Login/Login";
+import Request from '../views/Request/Request';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={IndexPage} />
     <Route path="/login" component={Login} />
+    <Route path="/request" component={Request} />
   </Switch>
 );
 export default Routes;
---
+++ src/views/Request/Request.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Request.scss";
+import RequestPanelHeader from '../../components/request-header/RequestPanelHeader';
+
+class Request extends Component{
+    render(){
+        return(
+          <RequestPanelHeader />
+        )
+    }
+}
+
+export default Request;
